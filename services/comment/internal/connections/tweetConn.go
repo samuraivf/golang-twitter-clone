3df814c7422fd87fe219faadc1059168397daf19
samuraivf <vovka4034@gmail.com>
2022-12-05 19:41:45 +0200

    Add transactions to comment repository

--- services/comment/internal/connections/tweetConn.go
+++ services/comment/internal/connections/tweetConn.go
@@ -1,13 +1,22 @@
-package repository
+package connections
 
 import (
+	tweetService "tweet/proto"
+
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/viper"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
-func ConnectTweetGrpc() *grpc.ClientConn {
+func GetTweetClient() (tweetService.TweetClient, func() error) {
+	connection := connectTweetGrpc()
+	tweetClient := tweetService.NewTweetClient(connection)
+
+	return tweetClient, connection.Close
+}
+
+func connectTweetGrpc() *grpc.ClientConn {
 	var connection *grpc.ClientConn
 	connection, err := grpc.Dial(viper.GetString("services.tweet"), grpc.WithTransportCredentials(insecure.NewCredentials()))
 	if err != nil {
--- services/comment/internal/repo/comment_postgres.go
+++ services/comment/internal/repo/comment_postgres.go
@@ -7,6 +7,7 @@ import (
 
 	"comment/dto"
 	"comment/internal/repo/models"
+	"comment/internal/connections"
 	messageService "message/proto"
 	tweetService "tweet/proto"
 
@@ -37,29 +38,33 @@ func (r *CommentPostgres) CreateComment(commentDto dto.CreateCommentDto) (uint, 
 		UserID:  commentDto.UserID,
 	}
 
-	result := r.db.Save(&comment)
-	if result.Error != nil {
-		return 0, result.Error
+	tx := r.db.Begin()
+	if tx.Error != nil {
+		return 0, tx.Error
 	}
 
-	tweetConnection := ConnectTweetGrpc()
-	defer tweetConnection.Close()
-
-	tweetClient := tweetService.NewTweetClient(tweetConnection)
-
-	ctx := context.Background()
-
-	if _, err := tweetClient.AddComment(ctx, &tweetService.CommentTweetId{
-		CommentId: uint64(comment.ID),
-		TweetId: uint64(comment.TweetID),
-	}); err != nil {
+	err := tx.Create(&comment).Error
+	if err != nil {
+		tx.Rollback()
 		return 0, err
 	}
 
-	messageConnection := ConnectMessageGrpc()
-	defer messageConnection.Close()
+	tweetClient, closeTweet := connections.GetTweetClient()
+	defer closeTweet()
 
-	messageClient := messageService.NewMessageClient(messageConnection)
+	ctx := context.Background()
+
+	_, err = tweetClient.AddComment(ctx, &tweetService.CommentTweetId{
+		CommentId: uint64(comment.ID),
+		TweetId: uint64(comment.TweetID),
+	})
+	if err != nil {
+		tx.Rollback()
+		return 0, err
+	}
+
+	messageClient, closeMessage := connections.GetMessageClient()
+	defer closeMessage()
 
 	message := messageService.MessageData{
 		Text:     fmt.Sprintf("@%s commented your tweet", commentDto.Username),
@@ -68,10 +73,17 @@ func (r *CommentPostgres) CreateComment(commentDto dto.CreateCommentDto) (uint, 
 		TweetId:  uint64(commentDto.TweetID),
 	}
 
-	if _, err := messageClient.AddMessage(ctx, &message); err != nil {
+	_, err = messageClient.AddMessage(ctx, &message)
+	if err != nil {
+		tx.Rollback()
 		return 0, err
 	}
 
+	err = tx.Commit().Error
+	if err != nil {
+		return 0, err
+	}
+	
 	return comment.ID, nil
 }
 
@@ -105,24 +117,35 @@ func (r *CommentPostgres) UpdateComment(commentDto dto.UpdateCommentDto) (uint, 
 }
 
 func (r *CommentPostgres) DeleteComment(id uint) error {
-	tweetConnection := ConnectTweetGrpc()
-	defer tweetConnection.Close()
+	tweetClient, closeTweet := connections.GetTweetClient()
+	defer closeTweet()
 
-	tweetClient := tweetService.NewTweetClient(tweetConnection)
+	tx := r.db.Begin()
+	if tx.Error != nil {
+		return tx.Error
+	}
 
-	if _, err := tweetClient.DeleteComment(context.Background(), &tweetService.CommentId{
+	_, err := tweetClient.DeleteComment(context.Background(), &tweetService.CommentId{
 		CommentId: uint64(id),
-	}); err != nil {
+	})
+	if err != nil {
+		tx.Rollback()
 		return err
 	}
 
-	return r.db.Delete(&models.Comment{}, id).Error
+	err = tx.Delete(&models.Comment{}, id).Error
+	if err != nil {
+		return err
+	}
+
+	return tx.Commit().Error
 }
 
 func (r *CommentPostgres) LikeComment(commentId, userId uint, username string) error {
 	var comment models.Comment
 
-	if err := r.db.Where("id = ?", commentId).Preload("Likes").First(&comment).Error; err != nil {
+	err := r.db.Where("id = ?", commentId).Preload("Likes").First(&comment).Error
+	if err != nil {
 		return err
 	}
 
@@ -132,17 +155,22 @@ func (r *CommentPostgres) LikeComment(commentId, userId uint, username string) e
 		}
 	}
 
-	if err := r.db.Model(&comment).Association("Likes").Append(&models.UserId{
+	tx := r.db.Begin()
+	if tx.Error != nil {
+		return tx.Error
+	}
+
+	err = tx.Model(&comment).Association("Likes").Append(&models.UserId{
 		UserID: userId,
 		CommentID: comment.ID,
-	}); err != nil {
+	})
+	if err != nil {
+		tx.Rollback()
 		return err
 	}
 
-	connection := ConnectMessageGrpc()
-	defer connection.Close()
-
-	messageClient := messageService.NewMessageClient(connection)
+	messageClient, closeMessage := connections.GetMessageClient()
+	defer closeMessage()
 
 	message := messageService.MessageData{
 		Text:     fmt.Sprintf("@%s liked your comment", username),
@@ -151,27 +179,44 @@ func (r *CommentPostgres) LikeComment(commentId, userId uint, username string) e
 		TweetId:  uint64(comment.TweetID),
 	}
 
-	if _, err := messageClient.AddMessage(context.Background(), &message); err != nil {
+	_, err = messageClient.AddMessage(context.Background(), &message)
+	if err != nil {
+		tx.Rollback()
 		return err
 	}
 
-	return nil
+	return tx.Commit().Error
 }
 
 func (r *CommentPostgres) UnlikeComment(commentId, userId uint) error {
 	var comment models.Comment
 
-	if err := r.db.Where("id = ?", commentId).Preload("Likes").First(&comment).Error; err != nil {
+	err := r.db.Where("id = ?", commentId).Preload("Likes").First(&comment).Error
+	if err != nil {
 		return err
 	}
 
 	for _, user := range comment.Likes {
 		if user.UserID == userId {
-			if err := r.db.Model(&comment).Association("Likes").Delete(user); err != nil {
+			tx := r.db.Begin()
+			if tx.Error != nil {
+				return tx.Error
+			}
+
+			err := tx.Model(&comment).Association("Likes").Delete(user)
+			if err != nil {
+				tx.Rollback()
 				return err
 			}
 
-			if err := r.db.Delete(user).Error; err != nil {
+			err = tx.Delete(user).Error
+			if err != nil {
+				tx.Rollback()
+				return err
+			}
+
+			err = tx.Commit().Error
+			if err != nil {
 				return err
 			}
 		}
